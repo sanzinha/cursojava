cc523482a494a0676d90346ba253210afe625bd0
samara <126591429+sanzinha@users.noreply.github.com>
2024-09-18 08:56:56 -0300

    ok

--- aula2.js
+++ aula2.js
@@ -1,12 +1,25 @@
-console.log("Meu Primeiro programa: trabalhando com variáveis");
+// Declaração de variáveis
 
-const idade = 26;
+let numero1 = 10; // Primeiro número
 
-console.log("idade");
-console.log(idade);
-console.log(idade+2);
-console.log(idade-2);
-console.log(idade/2);
+let número2 = 5; // Segundo número
 
-const idadeSomada = idade+2;
-console.log(idadeSomada);
+// Operadores aritméticos let soma number1 + number2;
+
+let soma = numero1 + numero2;
+
+let subtracao = numero1 - numero2;
+
+let multiplicacao = numero1 * numero2;
+
+let divisao = numero1 / numero2;
+
+// Exibe os resultados no console console.log("Soma: " + soma); console.log("Subtração:" + subtracao); console.log("Multiplicação: multiplicacao);
+
+console.log("Divisão:" + divisao);
+
+console.log("Subtração:" + subtracao);
+
+console.log("Multiplicação:" + multiplicacao);
+
+console.log("Divisão:" + divisao);
