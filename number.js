313451e9d22cd6cc7b87ae7b2cd703eb8e8978f3
samara <126591429+sanzinha@users.noreply.github.com>
2024-09-25 08:47:15 -0300

    ok

---
+++ number.js
@@ -0,0 +1,5 @@
+let raio = 7; // valor do raio 
+const PI = 3.14159; // constante para o valor de PI 
+let area = PI * raio * raio; // cálculo da área
+
+console.log("A área do círculo é: " + area);
