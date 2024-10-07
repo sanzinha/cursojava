18af0e9ae92ad804c1ead8484b7033448e692fac
samara <126591429+sanzinha@users.noreply.github.com>
2024-10-07 10:55:41 -0300

    ok

---
+++ exercicio 9.js
@@ -0,0 +1,27 @@
+// Definindo as variáveis
+const nome = "samara";
+const idade = 17;
+const inscrito = true;
+const curso = "Programação Web";
+
+// Criando o objeto usuário
+const usuario = {
+  nome: nome,
+  idade: idade,
+  inscrito: inscrito,
+  curso: curso
+};
+
+// Exibindo as informações no console
+console.log("Nome do usuário:", nome);
+console.log("Idade do usuário:", idade);
+console.log("Status de inscrição:", inscrito);
+console.log("Curso:", curso);
+console.log("Informações completas do usuário:", usuario);
+
+// Verificando se o usuário está inscrito
+if (usuario.inscrito) {
+  console.log(`${usuario.nome} está inscrito no curso de ${usuario.curso}.`);
+} else {
+  console.log(`${usuario.nome} não está inscrito em nenhum curso.`);
+}
